887d9c59d479f37c59913f3dc29a63cc12b80a94
dalbizurez <dalbizurez@correo.url.edu.gt>
2025-08-21 22:21:15 -0600

    [fix/post]: added id suppport implementation

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -3,11 +3,17 @@ import Description from "./postValueObjects/description";
 import Author from "./postValueObjects/author";
 
 export default class Post {
+
+    id: any|undefined;
+
     constructor(
         public title: Title,
         public description: Description,
-        public author: Author
-    ) {}
+        public author: Author,
+        id?: number
+    ) {
+        this.id = id;
+    }
 
     public static create(
         title: string,
